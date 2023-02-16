c335a07acdd780bb7ba068f3b01c7b4412d75f0b
rich8rds <rbukunmi8@gmail.com>
2023-02-16 10:06:35 +0100

    Edited home controller

--- src/main/java/com/richards/mealsapp/controller/HomeController.java
+++ src/main/java/com/richards/mealsapp/controller/HomeController.java
@@ -2,19 +2,20 @@ package com.richards.mealsapp.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class HomeController {
     @GetMapping
-    public String homepage(Principal principal) {
-        if(principal != null) log.info("Username: {}", principal.getName());
-        return "Welcome to the homepage!";
+    public Map<String, Object> homepage(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+        return oAuth2AuthenticationToken.getPrincipal().getAttributes();
     }
 
     @GetMapping("/user")
